1a3d67eba676707dd9b96e8ac884a171ac068845
Sydney <sdny7293@gmail.com>
2022-02-13 15:12:33 -0800

    mission page basic layout

--- client/navBar.php
+++ client/navBar.php
@@ -5,7 +5,7 @@
         <div class = "centerNav">
             <!-- . takes to current location, .. takes to parents (goes up one directory) -->
             <li class = "nav-list"><a class = "page"<?php if(basename($_SERVER["SCRIPT_FILENAME"], '.php') == 'home'){ echo "class='active'"; }?>href="home.php">Home</a></li>        
-            <li class = "nav-list"><a class = "page"<?php if(basename($_SERVER["SCRIPT_FILENAME"], '.php') == 'about'){ echo "class='active'"; }?>href="missions.php">Missions</a></li>
+            <li class = "nav-list"><a class = "page"<?php if(basename($_SERVER["SCRIPT_FILENAME"], '.php') == 'about'){ echo "class='active'"; }?>href="mission.php">Mission</a></li>
             <li class = "nav-list"><a class = "page"<?php if(basename($_SERVER["SCRIPT_FILENAME"], '.php') == 'team'){ echo "class='active'"; }?>href="team.php">Team</a></li>
             <li class = "nav-list"><a class = "page"<?php if(basename($_SERVER["SCRIPT_FILENAME"], '.php') == 'contact'){ echo "class='active'"; }?>href="gallery.php">Gallery</a></li>
             <li class = "nav-list"><a class = "page"<?php if(basename($_SERVER["SCRIPT_FILENAME"], '.php') == 'info'){ echo "class='active'"; }?>href="info.php">Info</a></li>
