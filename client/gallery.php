517ac4a723371bf10d8bc9773265325cd1d0dfe8
Sydney <sdny7293@gmail.com>
2022-02-13 15:17:05 -0800

    gallery page basic template

---
+++ client/gallery.php
@@ -0,0 +1,39 @@
+<?php
+session_start();
+?>
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    <title>Gallery</title>
+    <link rel="stylesheet" href="./css/gallery.css"/>
+    <link rel="stylesheet" href="./css/theme.css"/>
+    <!-- Nav bar css-->
+    <link rel="stylesheet" href="./css/navBar.css"/>
+    <!-- Nav bar arrow font -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <!-- Font style from https://fonts.google.com/specimen/Arvo?thickness=7&width=8&preview.text=COMING&preview.text_type=custom#standard-styles-->
+    <style>@import url('https://fonts.googleapis.com/css2?family=Arvo:wght@700&display=swap');</style>
+</head>
+
+<body>
+    <header>
+        <nav>
+            <!-- Navigation bar -->
+            <?php include 'navBar.php'?>
+        </nav>
+        <br><br><br>
+        <h1 id = "main-title">Gallery</h1>
+        <div class = "main-container">
+            <div class = "content">
+                <h2 class = "block-title">Place holder</h2>
+                <p class = "block-text">Welcome to the gallery</p>
+                <p class = "block-text">This is some more text</p>
+            </div>
+        </div>
+    </header>
+</body>
+<?php include 'footer.php'?>
+</html>
